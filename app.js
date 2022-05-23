f9aaf7a9411e3f773df07af8e47e2b820e4efa32
Pina Gomet <pina@Pinas-MacBook-Pro.local>
2022-05-22 21:17:29 -0400

    changed first line of code in node

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = (profileDataArr) => {
     for (let i = 0; i < profileDataArr.length; i++) {
