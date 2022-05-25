7d564cccc80e7fc9f4a574c043b8fbb7da359c72
Pina Gomet <pina@Pinas-MBP.lan>
2022-05-25 13:54:31 -0400

    created a new file, added an index.html

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require("./src/page-template");
+const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
 
-// const printProfileData = (profileDataArr) => {
-    // for (let i = 0; i < profileDataArr.length; i++) {
-        // console.log(profileDataArr[i]);
-    // }
 
-    // console.log("================");
 
-    // profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-// printProfileData(profileDataArgs);
-
-// const generatePage = () => "Name: Jane, GitHub: janehub";
-// console.log(generatePage());
-
-const generatePage = (userName, githubName) => {
-    return `
-      Name: ${userName}
-      GitHub: ${githubName}
-    `;
-};
-
-console.log(name, github);
-console.log(generatePage(name, github));
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
